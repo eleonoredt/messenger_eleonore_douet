38918a17c9491ccafa7a69cbe2c1cbaaa4e68963
Eleonore Douet <eleonore.douet@icloud.com>
2025-11-04 15:16:05 +0100

    'nouvel utilisateur debut nouveau groupe'

--- messenger.py
+++ messenger.py
@@ -19,6 +19,21 @@ server = {
     ]
 }
 
+ident=[]
+idgp=[]
+idpers=[]
+
+idnom = 0
+
+def get_id_from_name(nom):
+    for user in (server['users']):
+        if nom == user['name']:
+            idnom = user['id']
+        else:
+            idnom
+    return idnom
+
+
 def menu():
     print('=== Messenger ===')
     print('x. Leave')
@@ -26,47 +41,73 @@ def menu():
     print('gp. Afficher les groupes')
     print('b. Back to the menu')
     choice = input('Select an option: ')  
+    if choice == 'x':
+        print('Bye!')
+    elif choice == 'u':
+        users()
+        choice3 = input('b : go back to menu, n: create new user ')
+        if choice3 == 'b': 
+            menu()
+        elif choice3 == 'n': 
+            newuser()
+    elif choice == 'gp':
+        groupe()
+        choice2 = input('Select a group by its id: ')
+        for serv in (server['channels']) :
+            if choice2 == serv['id']:
+                affichegroupe()  
+            else: 
+                print('No group')
+        choicegp = input('Voulez vous creer un nouveau groupe? si oui tapez ng sinon tapez b pour revenir ')
+        if choicegp == 'ng':
+            newgp()
+        else :
+            menu()
+    elif choice == 'b':
+        menu()
+    else:
+        print('Unknown option:', choice)
 
-
-
-
-ident=[]
-
-print('=== Messenger ===')
-print('x. Leave')
-print('u. Afficher les utilisateurs')
-print('gp. Afficher les groupes')
-print('b. Back to the menu')
-choice = input('Select an option: ')
-if choice == 'x':
-    print('Bye!')
-elif choice == 'u':
+def users():
     for user in (server['users']) : 
         nomid = str(user['id']) + '. ' + user['name'] #nomid=f"{user['id']}. {user['name']}
         print(nomid)
-    choice3 = input('b : go back to menu, n: create new user ')
-    if choice3== 'b': 
-        menu()
-    elif choice3== 'n': 
-        nomnew=input('Donner le nom du nouvel utilisateur')
-        for user in (server['users']) : 
-            ident.append(user['id'])
-        newid=max(ident)+1
-        newuser={'id':newid , 'name': nomnew}
-elif choice == 'gp':
+
+def newuser():
+    nomnew = input('Donner le nom du nouvel utilisateur entre guillemets ')
+    for user in (server['users']) : 
+        ident.append(user['id'])
+    newid = max(ident) + 1
+    newuser = {'id':newid , 'name': nomnew}
+    server['users'].append(newuser)
+
+def groupe():
     for serv in (server['channels']) :
         groupe = str(serv['id']) + '. ' + serv['name']
         print(groupe)
-    choice2 = input('Select a group by its id: ')
-    for serv in (server['channels']) :
-        if choice2 == serv['id']:
-            for mess in server['messages']:
-                message= 'The sender id is ' + str(mess['sender_id'])+ '. They said: ' + mess['content']
-                print(message)
-        else: 
-            print('No group')
-elif choice == 'b':
-    menu()
-else:
-    print('Unknown option:', choice)
+
+def affichegroupe():
+    for mess in server['messages']:
+        message = 'The sender id is ' + str(mess['sender_id'])+ '. They said: ' + mess['content']
+        print(message)
+
+def newgp():
+    newnomgp = input('Donnez le nom du groupe entre guillemets ')
+    for user in (server['channels']) : 
+        idgp.append(user['id'])
+    idgpnew = max(idgp)
+    
+    nbpers = input('Combien de personnes dans le groupe? ')
+    if nbpers == 1: 
+        personnes = input('Donner le nom du nouvel utilisateur entre guillemets ')
+        idpers.append(get_id_from_name(personnes))
+    else: 
+        for i in nbpers: 
+            personnes = input('Donner le nom du premier utilisateur entre guillemets ')
+            idpers.append(get_id_from_name(personnes))
+    gpnew= {'id': idgpnew, 'name': 'newnomgp', 'member_ids': idpers}
+    server['channels'].append(gpnew)
+
+
+menu()
 
